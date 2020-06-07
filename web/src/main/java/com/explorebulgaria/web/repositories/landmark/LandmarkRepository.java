aec5b2ea993a9f1208966cb95d4a07d1bb874586
nellidimitrowa <nellidimitrowa@gmail.com>
2020-06-08 00:38:50 +0300

    Add logic for get and update landmark

--- web/src/main/java/com/explorebulgaria/web/repositories/landmark/LandmarkRepository.java
+++ web/src/main/java/com/explorebulgaria/web/repositories/landmark/LandmarkRepository.java
@@ -1,4 +1,12 @@
 package com.explorebulgaria.web.repositories.landmark;
 
+import com.explorebulgaria.web.models.Landmark;
+
+import java.util.List;
+
 public interface LandmarkRepository {
+
+    Landmark getLandmarkById(int id);
+    List<Landmark> getLandmarksByRegion(int regionId);
+    void updateLandmark(int landmarkId, Landmark landmark);
 }
--- web/src/main/java/com/explorebulgaria/web/repositories/landmark/SqlLandmarkRepository.java
+++ web/src/main/java/com/explorebulgaria/web/repositories/landmark/SqlLandmarkRepository.java
@@ -1,7 +1,64 @@
 package com.explorebulgaria.web.repositories.landmark;
 
+import com.explorebulgaria.web.models.Landmark;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class SqlLandmarkRepository implements  LandmarkRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Landmark getLandmarkById(int id) {
+        Landmark landmark;
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            landmark = session.createQuery("from Landmark where landmarkId = :id", Landmark.class)
+                    .setParameter("id", id).uniqueResult();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return landmark;
+    }
+
+    @Override
+    public List<Landmark> getLandmarksByRegion(int regionId) {
+        List<Landmark> landmarks;
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            landmarks = session.createQuery("from Landmark where regionId = :id", Landmark.class)
+                    .setParameter("id", regionId).list();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return landmarks;
+    }
+
+    @Override
+    public void updateLandmark(int landmarkId, Landmark landmark) {
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Landmark landmarkToBeUpdated = session.get(Landmark.class, landmarkId);
+
+            landmarkToBeUpdated.setIsVisited(landmark.getIsVisited());
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
