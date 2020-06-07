60b009518c1e35add92b29f13e1b7237b41c919d
nellidimitrowa <nellidimitrowa@gmail.com>
2020-06-08 00:54:25 +0300

    Add logic in controller classes

--- web/src/main/java/com/explorebulgaria/web/controllers/LandmarkRestController.java
+++ web/src/main/java/com/explorebulgaria/web/controllers/LandmarkRestController.java
@@ -1,4 +1,33 @@
 package com.explorebulgaria.web.controllers;
 
+import com.explorebulgaria.web.models.Landmark;
+import com.explorebulgaria.web.services.landmark.LandmarkService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/landmarks")
 public class LandmarkRestController {
+
+    private LandmarkService landmarkService;
+
+    public LandmarkRestController(LandmarkService landmarkService) {
+        this.landmarkService = landmarkService;
+    }
+
+    @GetMapping("/{landmarkId}")
+    public Landmark getLandmarkById(@PathVariable int landmarkId) {
+        return landmarkService.getLandmarkById(landmarkId);
+    }
+
+    @GetMapping("/{regionId}")
+    public List<Landmark> getLandmarksByRegionId(@PathVariable int regionId) {
+        return landmarkService.getLandmarksByRegion(regionId);
+    }
+
+    @PutMapping("/{id}")
+    public void updateLandmark(@PathVariable int id, @RequestBody Landmark landmark) {
+        landmarkService.updateLandmark(id, landmark);
+    }
 }
--- web/src/main/java/com/explorebulgaria/web/controllers/RegionRestController.java
+++ web/src/main/java/com/explorebulgaria/web/controllers/RegionRestController.java
@@ -1,5 +1,9 @@
 package com.explorebulgaria.web.controllers;
 
+import com.explorebulgaria.web.models.Region;
+import com.explorebulgaria.web.services.region.RegionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/regions")
 public class RegionRestController {
 
+    private RegionService regionService;
 
+    public RegionRestController(RegionService regionService) {
+        this.regionService = regionService;
+    }
 
+    @GetMapping("/{regionId}")
+    public Region getRegionById(@PathVariable int regionId) {
+        return regionService.getRegionById(regionId);
+    }
 }
