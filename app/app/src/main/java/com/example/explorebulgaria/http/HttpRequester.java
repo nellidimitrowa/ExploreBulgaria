7ab4fc1a14300f5556084a783eb47f9fe1b1c973
nellidimitrowa <nellidimitrowa@gmail.com>
2020-06-18 02:04:13 +0300

    Add reactiveX lib, delete Cardview lib, refactoring

--- app/app/src/main/java/com/example/explorebulgaria/http/HttpRequester.java
+++ app/app/src/main/java/com/example/explorebulgaria/http/HttpRequester.java
@@ -6,5 +6,5 @@ public interface HttpRequester {
 
     String get(String url) throws IOException;
 
-    String post(String url, String body) throws IOException;
+    String put(String url, String body) throws IOException;
 }
