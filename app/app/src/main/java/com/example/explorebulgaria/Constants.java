511e1d11724b377ede8400bdbbdeb39c662d90d3
nellidimitrowa <nellidimitrowa@gmail.com>
2020-06-24 09:50:32 +0300

    Add region constants

--- app/app/src/main/java/com/example/explorebulgaria/Constants.java
+++ app/app/src/main/java/com/example/explorebulgaria/Constants.java
@@ -3,4 +3,33 @@ package com.example.explorebulgaria;
 public class Constants {
 
     public static final String BASE_SERVER_URL = "192.168.231.2:8080/api";
+
+    public static final int BLAGOEVGRAD_ID = 1;
+    public static final int BURGAS_ID = 2;
+    public static final int VARNA_ID = 3;
+    public static final int VELIKO_TARNOVO_ID = 4;
+    public static final int VIDIN_ID = 5;
+    public static final int VRACA_ID = 6;
+    public static final int GABROVO_ID = 7;
+    public static final int DOBRICH_ID = 8;
+    public static final int KARDJALI_ID = 9;
+    public static final int KUSTENDIL_ID = 10;
+    public static final int LOVECH_ID = 11;
+    public static final int MONTANA_ID = 12;
+    public static final int PAZARDJIK_ID = 13;
+    public static final int PERNIK_ID = 14;
+    public static final int PLEVEN_ID = 15;
+    public static final int PLOVDIV_ID = 16;
+    public static final int RAZGRAD_ID = 17;
+    public static final int RUSE_ID = 18;
+    public static final int SILISTRA_ID = 19;
+    public static final int SLIVEN_ID = 20;
+    public static final int SMOLYAN_ID = 21;
+    public static final int SOFIYA_ID = 22;
+    public static final int STARA_ZAGORA_ID = 23;
+    public static final int TARGOVISHTE_ID = 24;
+    public static final int HASKOVO_ID = 25;
+    public static final int SHUMEN_ID = 26;
+    public static final int YAMBOL_ID = 27;
+
 }
