29631a08c7bb6ffe4337195931fe211831832d2a
nellidimitrowa <nellidimitrowa@gmail.com>
2020-07-03 01:13:18 +0300

    Database changes

--- db/databaseScript/databaseScript.sql
+++ db/databaseScript/databaseScript.sql
@@ -1,27 +1,29 @@
--- MySQL Workbench Forward Engineering
--- -----------------------------------------------------
--- Schema explorebulgaria
--- -----------------------------------------------------
+--- MySQL Workbench Forward Engineering
+--- -----------------------------------------------------
+--- Schema explorebulgaria
+--- -----------------------------------------------------
+-
 
 CREATE SCHEMA IF NOT EXISTS `explorebulgaria` DEFAULT CHARACTER SET utf8 ;
 USE `explorebulgaria`;
 
--- -----------------------------------------------------
--- Table `explorebulgaria`.`regions`
--- -----------------------------------------------------
+--- -----------------------------------------------------
+--- Table `explorebulgaria`.`regions`
+--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `explorebulgaria`.`regions` (
   `id` INT(10) NOT NULL AUTO_INCREMENT,
   `region_name` VARCHAR(45) NOT NULL,
   `latitude` DECIMAL(10, 8) NOT NULL,
   `longitude` DECIMAL(11, 8) NOT NULL,
+  `is_visited` TINYINT(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `IdRegion_UNIQUE` (`id` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
--- -----------------------------------------------------
--- Table `explorebulgaria`.`landmarks`
--- -----------------------------------------------------
+--- -----------------------------------------------------
+--- Table `explorebulgaria`.`landmarks`
+--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `explorebulgaria`.`landmarks` (
   `id` INT(10) NOT NULL AUTO_INCREMENT,
   `landmark_name` VARCHAR(100) NOT NULL,
